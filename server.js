94993254fd4057063b13a57efcd995e95df79626
Bishti04062005 <bisahasamanta1120@gmail.com>
2026-04-08 01:24:55 +0530

    fix: replace bcrypt with bcryptjs for Vercel Node.js 24 compatibility

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { GoogleGenAI } = require('@google/genai');
 const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
